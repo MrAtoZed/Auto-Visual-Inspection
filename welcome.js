9b32bdb7647806165548f83d036f3e79b3b47a56
MrAtoZed <120002590+MrAtoZed@users.noreply.github.com>
2024-06-09 11:50:04 +0700

    Add files via upload

---
+++ welcome.js
@@ -0,0 +1,5 @@
+function handleMenuClick(menu) {
+    alert(`You selected ${menu}`);
+    // Anda bisa menambahkan logika lain seperti pengalihan ke halaman lain
+    // Misalnya: window.location.href = `${menu}.html`;
+}
